f2835e855c192b1f4ff583ebce74e47dd7ceb80a
Verp <138831106+itzVerp@users.noreply.github.com>
2025-08-19 22:42:53 -0300

    Add files via upload

---
+++ file_line_cleaner.py
@@ -0,0 +1,14 @@
+# Ask user for input filename.
+# Open and read lines.
+# Clean them.
+# Save to new file.
+
+# Cleaning process:
+# Remove empty lines.
+# Remove extra spaces.
+# Lower case.
+# Remove Punctuation.
+# Remove digits.
+# Replace multiple spaces with one.
+# Remove special characters.
+# Remove duplicates.
