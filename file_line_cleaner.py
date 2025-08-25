7840afe8419494d802895499b8f2e5255af29005
Verp <138831106+itzVerp@users.noreply.github.com>
2025-08-24 22:08:00 -0300

    Update file_line_cleaner.py

--- file_line_cleaner.py
+++ file_line_cleaner.py
@@ -12,3 +12,22 @@
 # Replace multiple spaces with one.
 # Remove special characters.
 # Remove duplicates.
+import re
+
+with open("text.txt", "r", encoding="utf-8") as infile:  
+    lines = infile.readlines()
+
+cleaned_lines = []
+for line in lines:
+    
+    line = line.strip()
+    
+    if not line:
+        continue
+    
+    # Remove extra spaces
+    line = re.sub(r" +", " ", line).strip()
+    cleaned_lines.append(line + "\n")
+
+with open("cleaned_file.txt", "w", encoding="utf-8") as outfile:
+    outfile.writelines(cleaned_lines)
