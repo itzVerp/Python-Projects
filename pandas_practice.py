2945558842ab29e903dc380c501fbca4393e3fe8
Verp <138831106+itzVerp@users.noreply.github.com>
2025-09-02 21:45:28 -0300

    Add files via upload

---
+++ pandas_practice.py
@@ -0,0 +1,18 @@
+# 1. Load the file into a DataFrame and print the first 5 rows.
+# 2. Save the DataFrame to a new CSV file called employees_copy.csv.
+# 3. Show only the Name column.
+# 4. Show the rows for the first three employees.
+# 5. Show all employees who work in the "Finance" department.
+# 6. Add a new column that calculates each person’s salary after a 10% raise.
+# 7. Find the average salary per department.
+# 8. Show all employees sorted by age, oldest first.
+# 9. Show the dataset where missing values in the Age column are replaced with 0.
+# 10. Using this second dataset:
+
+# Department,Location
+# HR,New York
+# Finance,London
+# IT,San Francisco
+
+# combine it with the employees dataset so that each employee also has a location.
+
